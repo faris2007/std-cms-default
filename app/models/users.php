c204fdfcb7ef043954b44eba2e20ed4ebccbfc09
faris2007 <faris.php@gmail.com>
2012-12-29 22:48:59 +0300

    edit model users

--- app/models/users.php
+++ app/models/users.php
@@ -16,6 +16,81 @@ class users extends CI_Model{
     private function encrypt_password($pass){
         return sha1($pass);
     }
+    
+    private function setSession($userid,$groupId){
+        $data = $this->getPermissions($groupId);
+        $data['userid'] = $userid;
+        $data['group'] = $groupId;
+        $this->session->set_userdata($data);
+    }
+    
+    private function unsetSession(){
+        
+        $data['premissions'] = '';
+        $data['userid'] = '';
+        $data['group'] = '';
+        $this->session->unset_userdata($data);
+    }
+    
+    private function getPermissions($groupId)
+    {
+        $CI =& get_instance();
+        $CI->load->model('permissions');
+        $permissions = $CI->permissions->getPermissions($groupId);
+        if(!is_bool($permissions))
+        {
+            $data = array();
+            foreach ($permissions as $row)
+            {
+                if($row->function_name == "all")
+                    $data['permissions'][$row->service_name] =  true;
+                else
+                {
+                    if($row->value == "all")
+                        $data['permissions'][$row->service_name][$row->function_name] = true;
+                    else
+                    {
+                        $data['permissions'][$row->service_name][$row->function_name][$row->value] = true;
+                    }
+                }
+            }
+            return $data;
+        }else
+            return array(
+                'permissions' => false
+                );
+    }
+    
+    public function checkIfHavePremission($service_name = "admin",$function_name = "all",$value = "all",$otherValue = "all")
+    {
+        if(empty($service_name) || empty($function_name) || empty($value) || empty($otherValue))
+            return FALSE;
+        
+        $premission = $this->session->userdata('permissions');
+        $accessGrade = (isset($premission[$service_name]))?1:0;
+        $accessAdmin = (isset($premission[$service_name]))?1:4;
+        
+        if($function_name != "all"){
+            $accessGrade++;
+            if($value != "all") { 
+                $accessGrade++;
+                if($otherValue != "all") 
+                    $accessGrade++;
+            }
+        } 
+        if($accessAdmin == 1){
+            if(!is_bool($premission[$service_name])){
+                $accessAdmin++;
+                if(!is_bool($premission[$service_name][$function_name])) { 
+                    $accessAdmin++;
+                    if(!is_bool($premission[$service_name][$function_name][$value])) 
+                        $accessAdmin++;
+                }
+            }
+        }
+        $this->setSession($this->session->userdata('userid'),$this->session->userdata('group'));
+        return ($accessGrade >= $accessAdmin)? true:false;
+    }
 
 
     public function addNewUser($data){
@@ -115,6 +190,60 @@ class users extends CI_Model{
         }
     }
     
+    public function logout(){
+        $this->unsetSession();
+    }
+    
+    public function isLogin($redirect = FALSE)
+    {
+        if ($this->session->userdata('userid'))
+        {
+            return TRUE;
+        }else{
+            if ($redirect)
+            {
+                redirect();
+            }else{
+                return FALSE;
+            }
+        }
+    }
+    
+    public function change_password($username,$oldPass,$newPass,$admin = false){
+        if(empty($username)||(empty($oldPass)&& !$admin)||empty($newPass))
+            return false;
+        if(!$admin){
+            $this->db->where("password",  $this->encrypt_password($oldPass));
+        }
+        $this->db->where("username",$username);
+        $query = $this->db->get($this->_tables['users']);
+        if($query->num_rows() == 0)
+            return false;
+        $result = $query->row();
+        $data['password'] = $this->encrypt_password($newPass);
+        if($this->updateUser($result->id, $data)){
+            return true;
+        }else
+            return false;
+            
+    } 
+    
+    public function resetPassword($email){
+        if(empty($email))
+            return false;
+        $this->db->where("email",  $email);
+        $query = $this->db->get($this->_tables['users']);
+        if($query->num_rows() == 0)
+            return false;
+        $result = $query->row();
+        $newPassword = md5(rand(0, 100000)*time());
+        $data['password'] = $this->encrypt_password($newPassword);
+        if($this->updateUser($result->id, $data)){
+            return true;
+        }else
+            return false;
+            
+    }
     
     
 }
