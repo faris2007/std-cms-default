8e4e01927883670359d7f120a10a9b87d4a013a2
s3do <s3do@s3do-Satellite-E105>
2013-01-02 04:58:10 +0300

    last files

---
+++ app/models/password_log.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+class Password_Log extends CI_Model {
+      
+    private $_table = "password_log";
+    
+    function __construct() {
+        
+        parent::__construct();
+        
+    }
+            /*
+             *
+             */
+    function addNewLog($data) {
+        if( !is_array($data))
+            return false;
+        return $this->db->insert($this->_table,$data);
+    }
+    
+    function updateLog($id , $data) {
+        
+        if(empty($id) || !is_array($data))
+            return FALSE;
+        
+        $this->db->where('id' , $id);
+        
+        return $this->db->update($this->_table,$data);
+    }
+
+    
+     function deleteLog($id = "all") {
+        
+        if(empty($id) || !is_numeric($id))
+            return FALSE;
+        
+        $this->db->where('id',$id);
+        
+        return $this->db->delete($this->_table);
+            
+    }
+    
+    function getLogs($userid) {
+   
+         if(empty($userid) ) return false;
+        
+        if(is_numeric($userid))
+        {
+            $this->db->where('users_id',$userid);
+        }
+        $this->db->order_by("id"); 
+        $query = $this->db->get($this->_table);
+        return ($query->num_rows() > 0)? $query->result() : false;
+    
+    }
+    
+    function getLog($id) {
+        if(empty($id) || !is_numeric($id)) return false;
+        
+        $this->db->where('id' , $id);
+        
+        $query=  $this->db->get($this->_table);
+        
+        return( $query->num_rows() > 0) ?$query->row() :FALSE;
+        
+    }
+    
+}
+
+?>
---
+++ app/models/setting.php
@@ -0,0 +1,56 @@
+<?php
+
+class setting extends CI_Model {
+
+    private $_table = "setting";
+    
+    function __construct() {
+        
+        parent::__construct();
+    }
+
+    function addNewSetting($data) {
+        if(!is_array($data))
+            return FALSE;
+        
+      return $this->db->insert($this->_table, $data); 
+    }
+
+    function updateSetting($id , $data) {
+        if(empty($id) || !is_array($data))
+            return FALSE;
+                return $this->db->insert($this->_table, $data); 
+
+            
+    }
+    
+    function deleteSetting($id , $data) {
+        if (empty($id) || !is_array($data))
+            return FALSE;
+    
+        $this->db->where('id',$id);
+        return $this->db->delete($this->_table); 
+    }
+
+    function getSettings($userid) {
+        if(empty($userid)) return false;
+        
+        if(is_numeric($userid))
+        {
+            $this->db->where('users_id',$userid);
+        }
+        $this->db->order_by("id"); 
+        $query = $this->db->get($this->_table);
+        return ($query->num_rows() > 0)? $query->result() : false;
+    }
+    
+    function getSetting($id) {
+           if(empty($id) || !is_numeric($id)) return false;
+        
+        $this->db->where('id',$id);
+        $query = $this->db->get($this->_table);
+        return ($query->num_rows() > 0)? $query->row() : false; 
+    }
+}
+
+?>
